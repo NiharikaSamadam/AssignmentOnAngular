9344b476c917c6726934fb307488ba66c8a60b28
niharika <samadamniharika@gmail.com>
2021-03-17 20:13:15 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ 
+  username:string
   title = 'ecommerceapp';
+  constructor(private router : Router){}
+  ngOnInit(): void {
+    this.username = localStorage.getItem("username")
+  }
+
+  logout(){
+    localStorage.clear()
+    this.router.navigateByUrl("/login")
+  }
 }
